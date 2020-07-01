50d1427cf08b62ec69a0f467fdff9530f7ef93d7
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2020-07-01 10:00:57 -0300

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Bruno Cardoso Ambrosio on 01/07/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
